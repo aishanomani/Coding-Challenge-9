c08edd0329fafcca1a7dee2dda2bcb3425645fad
Aisha Nomani <aishan@usf.edu>
2025-02-21 18:58:01 -0500

    Task 1 - Created Employee Class

---
+++ cc_9.js
@@ -0,0 +1,20 @@
+// Task 1: Creating an Employee Class
+console.log("********** Created Employee Class **********");
+class Employee {
+    constructor(name, id, department, salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+    getDetails() {
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`;
+    }
+    calculateAnnualSalary() {
+        return this.salary * 12;
+    }
+}
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+console.log(emp1.getDetails()); // Expected output
+console.log(emp1.calculateAnnualSalary()); // Expected output
+
